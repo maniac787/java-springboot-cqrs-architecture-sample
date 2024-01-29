98e608d7e5550622aac5bdba3a777f163d04662c
Roberto Chasipanta <roberto.chasipanta@globant.com>
2024-01-29 14:02:09 -0500

    feat: refactor to gradle

--- src/main/java/com/bac/sample/hexagonal/infra/inputport/OrderInputPort.java
+++
@@ -1,9 +0,0 @@
-package com.bac.sample.hexagonal.infra.inputport;
-
-import com.bac.sample.hexagonal.domain.Orders;
-
-import java.math.BigDecimal;
-
-public interface OrderInputPort {
-    Orders createOrder(String customerId, BigDecimal total);
-}
